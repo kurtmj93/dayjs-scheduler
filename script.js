54161f897f932701f10935f4e891f5d6c3cdcde3
Kurtmj93 <kurtmj93@gmail.com>
2022-12-19 20:59:39 -0500

    select textareas by parent div id and getItem from localStorage

--- script.js
+++ script.js
@@ -16,16 +16,22 @@ $(function () {
       });
 
 
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+  $(".time-block").each(function() {
+    $(this).find('textarea').text(localStorage.getItem($(this).attr('id')));
+  });
+  
   // TODO: Add code to display the current date in the header of the page.
 });
